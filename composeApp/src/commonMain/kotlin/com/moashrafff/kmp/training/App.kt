f512727797e27e5e2f289740ebc6f46e6d744dc5
mohamedashraf <mohamed.ashraf.rady0@gmail.com>
2025-06-25 21:48:14 +0300

    sharing resources and string localization

--- composeApp/src/commonMain/kotlin/com/moashrafff/kmp/training/App.kt
+++ composeApp/src/commonMain/kotlin/com/moashrafff/kmp/training/App.kt
@@ -1,12 +1,16 @@
 package com.moashrafff.kmp.training
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kmptraining.composeapp.generated.resources.Res
+import kmptraining.composeapp.generated.resources.hello_world
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -15,9 +19,12 @@ fun App(
     batteryManager: BatteryManager
 ) {
     MaterialTheme {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column (modifier = Modifier.fillMaxSize()) {
             Text(
                 text = "The current battery level is ${batteryManager.getBatteryLevel()}%"
+            )
+            Text(
+                text = stringResource(Res.string.hello_world)
             )
         }
     }
